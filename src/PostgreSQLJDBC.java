4da944e97481fb9d80e52d0deea44a5a297c889f
HimadriBanerjee <himadribanerjee98@gmail.com>
2020-11-21 14:28:33 +0530

    fourth commit

--- src/PostgreSQLJDBC.java
+++ src/PostgreSQLJDBC.java
@@ -10,18 +10,17 @@ public class PostgreSQLJDBC {
             c = PostgresManager.getConnection();
             c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Shivaji@16");
             Statement statement = c.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from Passenger");
+            ResultSet resultSet = statement.executeQuery("select * from Passenger" );
             while (resultSet.next()) {
+                System.out.println("idCard: " + resultSet.getLong("idCard"));
                 System.out.println("name: " + resultSet.getString("name"));
                 System.out.println("age: " + resultSet.getInt("age"));
                 System.out.println("address: " + resultSet.getString("address"));
-                System.out.println("contact: " + resultSet.getInt("idCard"));
                 System.out.println("contact: " + resultSet.getLong("contact"));
                 System.out.println("==========================");
             }
-        }
-        catch(Exception e){
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+}

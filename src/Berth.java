b6d9471b13e8754bc2a6d5028a5b2102e3d551a1
LUV AGARWAL <luvluv.a7@gmail.com>
2020-11-21 18:58:34 +0530

    Luv commit

--- src/Berth.java
+++ src/Berth.java
@@ -1,33 +1,47 @@
 import java.util.Scanner;
+import java.util.Date;
+
+
 public class Berth {
     private static int[] berths = new int[40];
 
     public static void main(String args[]) {
+
+        System.out.println("Welcome to the Indian Railways! ");
+
+
         for (int i = 0; i < 40; i++) {
             berths[i] = 0;
         }
         Scanner s = new Scanner(System.in);
-        int choice;
+        int choice = 1;
+
         System.out.println("Please enter 1 for lower berth,2 for upper berth,0 to exit: ");
         choice = s.nextInt();
+
         while (choice != 0) {
             int berthnumber = 0;
+
             if (choice == 1) {
                 berthnumber = bookLowerberth();
-                if (berthnumber == 2) {
+                if (berthnumber == -1) {
                     berthnumber = bookUpperberth();
-                    if (berthnumber != 0) {
+
+                    if (berthnumber != -1 ) {
                               System.out.println("Sorry, we have no lower berth.Do you want upper berth?");
                         printBoardingPass(berthnumber);
-                    } else {
+                    } }
+                else {
                         System.out.println("lower berth available");
                         printBoardingPass(berthnumber);
-                    }
-                    if (choice == 2) {
+                    }}
+                    else if (choice == 2) {
                         berthnumber = bookUpperberth();
-                        if (berthnumber == 2) {
+
+                        if (berthnumber == -1) {
                             berthnumber = bookLowerberth();
-                            if (berthnumber != 0) {
+
+                            if (berthnumber != -1) {
                                 System.out.println("Sorry we are unable to book Upper berth.Do you want lower berth?");
                                 printBoardingPass(berthnumber);
                             }
@@ -35,7 +49,8 @@ public class Berth {
                             System.out.println("Upper berth available");
                             printBoardingPass(berthnumber);
                         }
-                    } else {
+                    }
+                    else {
                         System.out.println("Invalid choice made.Please try again");
                         choice = 0;
                     }
@@ -47,8 +62,7 @@ public class Berth {
                     choice = s.nextInt();
                 }
             }
-        }
-    }
+
     private static int bookLowerberth() {
         for (int i = 0; i < 20; i++) {
             if (berths[i] == 0) {
@@ -68,7 +82,11 @@ public class Berth {
     return-1;
 }
     private static void printBoardingPass(int berthnumber) {
-        System.out.println("Boarding pass for berth number" + berthnumber);
+        Date timenow = new Date();
+        System.out.println();
+        System.out.println("Boarding pass for berth number : " + berthnumber);
+
+        System.out.println("Ticket Booking Time : " + timenow.toString());
         System.out.println("This ticket is non transferable.");
         System.out.println("Please be courteous.Do not smoke.");
         System.out.println("Enjoy your journey.");
